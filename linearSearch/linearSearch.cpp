f708b93bf398fb46f6b447b10e6884db61d0a44a
Bangwahyu14 <whagung553@gmail.com>
2024-04-05 14:21:13 +0700

    membuat step 2

--- linearSearch/linearSearch.cpp
+++ linearSearch/linearSearch.cpp
@@ -42,5 +42,10 @@ void LinearSearch()
 	{
 		//Accept the nuber to be searched
 		cout << "\nEnter the element you want to search: "; //step 1
+		cin >> item;
+
+		ctr = 0;
+		i = 0;// step 2
+       
 	}
 }
